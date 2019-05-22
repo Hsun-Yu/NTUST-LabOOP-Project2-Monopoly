cb0c7b281e3457705da5efe03fb524d99d542224
張信中 <hcjohn463@gmail.com>
2019-05-22 13:04:15 +0800

    edit title

--- Monopoly/Monopoly/Game.cpp
+++ Monopoly/Monopoly/Game.cpp
@@ -5,7 +5,7 @@ COORD Game::cursorXY;
 
 Game::Game()
 {
-	// PlaySound("Music\\background_sound.wav", NULL, SND_FILENAME | SND_LOOP | SND_ASYNC);
+	PlaySound("Music\\background_sound.wav", NULL, SND_FILENAME | SND_LOOP | SND_ASYNC);
 	Game::enterScreen();
 }
 
@@ -22,14 +22,20 @@ void Game::setTextStyle(int color, int backgroundColor)
 void Game::enterScreen()
 {
 	system("cls");
-	cout << "                  __  __                                                            __            " << endl;
-	cout << "                  F  \\/  ]      ____      _ ___       ____      _ ___       ____     LJ    _    _  " << endl;
-	cout << "                 J |\\__/| L    F __ J    J '__ J     F __ J    J '__ J     F __ J    FJ   J |  | L " << endl;
-	cout << "                 | |`--'| |   | |--| |   | |__| |   | |--| |   | |--| |   | |--| |  J  L  | |  | | " << endl;
-	cout << "                 F L    J J   F L__J J   F L  J J   F L__J J   F L__J J   F L__J J  J  L  F L__J J " << endl;
-	cout << "                J__L    J__L J\\______/F J__L  J__L J\\______/F J  _____/L J\\______/F J__L  )-____  L" << endl;
-	cout << "                |__L    J__|  J______F  |__L  J__|  J______F  |_J_____F   J______F  |__| J\\______/F" << endl;
-	cout << "                                                              L_J                         J______F " << endl;
+	vector <string> title;
+	title = { "                  __  __                                                            __            " ,
+	"                  F  \\/  ]      ____      _ ___       ____      _ ___       ____     LJ    _    _  " ,
+	"                 J |\\__/| L    F __ J    J '__ J     F __ J    J '__ J     F __ J    FJ   J |  | L " ,
+	"                 | |`--'| |   | |--| |   | |__| |   | |--| |   | |--| |   | |--| |  J  L  | |  | | " ,
+	"                 F L    J J   F L__J J   F L  J J   F L__J J   F L__J J   F L__J J  J  L  F L__J J " ,
+	"                J__L    J__L J\\______/F J__L  J__L J\\______/F J  _____/L J\\______/F J__L  )-____  L" ,
+	"                |__L    J__|  J______F  |__L  J__|  J______F  |_J_____F   J______F  |__| J\\______/F" ,
+	"                                                              L_J                         J______F " };
+	for (int i = 0; i < title.size(); i++)
+	{
+		Game::setTextStyle(KHIKI, BLACK);
+		cout << title[i] << endl;
+	}
 
 	vector<string> option;
 	option = { " ＿＿＿＿＿＿＿＿ " ,
